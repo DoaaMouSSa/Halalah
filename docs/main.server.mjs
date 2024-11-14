2970545d18e60a15cc610bfc7cd66ceb2820e62c
doaamoussa5 <doaamoussa5@gmail.com>
2024-11-14 10:52:37 +0200

    up

--- docs/main.server.mjs
+++ docs/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-C763UQQO.mjs";import"./chunk-OEZOOQJY.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-YZPDZDLB.mjs";import"./chunk-OEZOOQJY.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
