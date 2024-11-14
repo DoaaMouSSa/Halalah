c07ee37f189bbcceaceca05fbd146ce7c837cba4
doaamoussa5 <doaamoussa5@gmail.com>
2024-11-14 15:08:28 +0200

    up

--- src/app/services/banner/banner.service.ts
+++ src/app/services/banner/banner.service.ts
@@ -1,10 +1,12 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment.prod';
 @Injectable({
   providedIn: 'root'
 })
 export class BannerService {
+  private apiUrl = environment.apiUrl;
 
   constructor(private _http: HttpClient) {}
 
--- src/app/services/coupon/coupon.service.ts
+++ src/app/services/coupon/coupon.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment.prod';
 @Injectable({
   providedIn: 'root'
 })
 export class CouponService {
+  private apiUrl = environment.apiUrl;
+
   constructor(private _http: HttpClient) {}
 
   getData() : Observable<any>{
