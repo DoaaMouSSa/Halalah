007bb58bbbce58200f08c85f50a605ae86f2a716
doaamoussa5 <doaamoussa5@gmail.com>
2025-08-08 00:54:23 +0300

    up

--- docs/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-BJN6BTFT.mjs";import"./chunk-5L3MWSJW.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-7WHNETHK.mjs";import"./chunk-5L3MWSJW.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
